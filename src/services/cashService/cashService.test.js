09110cd108b87ab77506f00dbb2b7b2315e399b1
Levanova Evgeniia <e.levanova@andersenlab.com>
2021-07-09 16:29:19 +0300

    fetch Data

--- src/services/cashService/cashService.test.js
+++ src/services/cashService/cashService.test.js
@@ -1,7 +1,28 @@
+const { fetchData } = require('../../utils/fetchData/fetchData');
 const { cashService } = require('./cashService');
 
+jest.mock('../../utils/fetchData/fetchData');
+
 describe('cashService', () => {
-  test('cashService returns object', () => {
-    expect(cashService()).toBeTruthy();
+  describe('fetchCashIn', () => {
+    test('should return fetchData response', () => {
+      fetchData.mockImplementation(() => ({ key: 'key ' }));
+
+      expect(cashService().fetchCashIn()).toEqual({ key: 'key ' });
+    });
+  });
+  describe('fetchCashOutNatural', () => {
+    test('should return fetchData response', () => {
+      fetchData.mockImplementation(() => ({ key: 'key ' }));
+
+      expect(cashService().fetchCashOutNatural()).toEqual({ key: 'key ' });
+    });
+  });
+  describe('fetchCashOutJuridical', () => {
+    test('should return fetchData response', () => {
+      fetchData.mockImplementation(() => ({ key: 'key ' }));
+
+      expect(cashService().fetchCashOutJuridical()).toEqual({ key: 'key ' });
+    });
   });
 });
--- src/utils/fetchData/fetchData.test.js
+++ src/utils/fetchData/fetchData.test.js
@@ -5,15 +5,12 @@ jest.mock('node-fetch-cache');
 
 describe('fetchData', () => {
   test('fetchData returns object', () => {
-    fetch.mockImplementation(() => ({
-      max: {
-        amount: 5,
-        currency: 'EUR',
-      },
-      percents: 0.03,
-    }));
     const obj = { max: { amount: 5, currency: 'EUR' }, percents: 0.03 };
-    expect(fetchData('cash-in')).resolves.toStrictEqual(obj);
+    fetch.mockImplementation(() => new Promise((resolve) => {
+      resolve(obj);
+    }));
+
+    expect(fetchData('cash-in')).resolves.toEqual(obj);
   });
   test('fetchData throws error if wrong endpoint', async () => {
     fetch.mockImplementation(() => new Error());
